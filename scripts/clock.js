f0d273bd63f2f2a400a05daefcd8e7f53b91ce85
gremblotech <04zcarpenter@gmail.com>
2025-09-13 14:33:38 -0400

    Cleaned clock.js code

--- scripts/clock.js
+++ scripts/clock.js
@@ -2,27 +2,18 @@ function updateTime() {
   let time = new Date();
   let hour = time.getHours();
   let min = time.getMinutes();
-  let period = getPeriod(hour);
+  let period = "AM"; 
 
-  hour = twelveHour(hour);
+  if (hour >= 12) {
+    period = "PM";
+    hour -= 12;
+  }
+
+  hour = hour === 0 ? 12 : hour; // Sets 0 AM/PM to 12
   min = min < 10 ? `0${min}` : min; // e.g. converts 9 to 09
 
   document.querySelector(".js-hours").innerHTML = hour;
   document.querySelector(".js-minuteperiod").innerHTML = `${min} ${period}`;
-}
-
-function twelveHour(hours) {
-  if (hours === 0) hours += 12;
-  else if (hours > 12) hours -= 12;
-
-  return hours;
-}
-
-function getPeriod(hours) {
-  let period = "AM";
-  if (hours >= 12) period = "PM";
-  
-  return period;
 }
 
 updateTime();
