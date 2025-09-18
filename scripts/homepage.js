cc31eec9b8bea6deb059bf6f900439e81d1336f2
gremblotech <04zcarpenter@gmail.com>
2025-09-17 23:05:30 -0400

    Added window logic

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,6 +1,41 @@
-async function createWindow(tab) {
-  let page = await fetch("news-latest.html");
-  let text = await page.text();
+const win = document.createElement("div");
+document.querySelector(".desktop").appendChild(win);
+win.innerHTML = "";
 
-  console.log(text);
+function makeWin(content) {
+  document.querySelector(".desktop").appendChild(win);
+
+  win.classList.add("window-container");
+  win.innerHTML = `
+    <div class="title-bar">
+      <div class="title-bar-title">
+        <img src="images/${content}.ico" />
+        <p>${content}</p>
+      </div>
+      <div class="title-bar-right">
+        <button></button>
+        <button></button>
+        <button></button>
+      </div>
+    </div>
+    <embed type="text/html" />
+  `;
 }
+
+function makeTab(content) {
+  if (win.innerHTML === "") makeWin(content);
+
+  if (content === "News") {
+    win.querySelector("embed").src = "news/latest.html";
+  }
+}
+
+function deleteWin() {
+  win.innerHTML = "";
+}
+
+document.querySelectorAll(".desktop-icon").forEach((icon) => {
+  icon.addEventListener("click", () => {
+    makeTab(`${icon.querySelector("p").innerHTML}`);
+  });
+});
