f18722ab70dd6ee7a4fd8f11907c9536ede6762e
gremblotech <04zcarpenter@gmail.com>
2026-07-21 23:53:21 -0400

    local storage for 1d bingo

--- games/scripts/1d-bingo.js
+++ games/scripts/1d-bingo.js
@@ -2,40 +2,64 @@ const FREESPACE = "TETRIS";
 
 async function getBingoPrompts() {
   const jsonfile = await fetch("scripts/bingoprompts.json");
-  const newfile = await jsonfile.json();
+  const res = await jsonfile.json();
 
-  return newfile;
+  return res;
 }
 
-function randomBingoArray(prompts) {
-  // shuffle
-  for (let i = prompts.length - 1; i > 0; i--) {
+function makeBingoString(prompts) {
+  let numbers = new Uint8Array(prompts.length);
+  for (let i = 0; i < numbers.length; i++) {
+    numbers[i] = i;
+  }
+
+  for (let i = numbers.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [prompts[i], prompts[j]] = [prompts[j], prompts[i]];
+    [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
   }
 
-  // prune
-  while (prompts.length > 24) {
-    prompts.pop();
+  while (numbers.length > 24) {
+    numbers.pop();
   }
 
-  prompts.splice(12, 0, FREESPACE);
+  localStorage.setItem("activeCells", 0); // bithacks incoming
 
-  return prompts;
+  let res = numbers.toBase64({ omitPadding: true });
+  localStorage.setItem("bingoString", res);
+  return res;
 }
 
-function markCell(element) {
-  if (element.classList.contains("unmarked-cell")) {
-    element.classList.remove("unmarked-cell");
-    element.classList.add("marked-cell");
-  } else {
-    element.classList.remove("marked-cell");
-    element.classList.add("unmarked-cell");
+function bingoArrayFromString(string, prompts) {
+  let numbers = Uint8Array.fromBase64(string);
+  let res = new Array(24);
+
+  for (let i = 0; i < res.length; i++) {
+    res[i] = prompts[numbers[i]];
   }
+
+  res.splice(12, 0, FREESPACE);
+
+  return res;
+}
+
+function markCell() {
+  let actives = parseInt(localStorage.getItem("activeCells"));
+  let idx = parseInt(this.id);
+  if (this.classList.contains("unmarked-cell")) {
+    this.classList.remove("unmarked-cell");
+    this.classList.add("marked-cell");
+    actives += 1 << idx;
+  } else {
+    this.classList.remove("marked-cell");
+    this.classList.add("unmarked-cell");
+    actives -= 1 << idx;
+  }
+  localStorage.setItem("activeCells", actives);
 }
 
 function throwBoardToDOM(bingoArray) {
   const table = document.querySelector(".board");
+  let actives = parseInt(localStorage.getItem("activeCells"));
 
   let row;
   let data;
@@ -47,9 +71,16 @@ function throwBoardToDOM(bingoArray) {
     row.classList.add("row", `row-${y}`);
     for (let x = 0; x < 5; x++) {
       data = document.createElement("td");
-      data.classList.add("cell", `cell-${y * 5 + x}`, "unmarked-cell");
+      data.classList.add("cell");
+      data.id = `${y * 5 + x}`;
+      if (((actives >> (y * 5 + x)) & 1) === 1) {
+        data.classList.add("marked-cell");
+      } else {
+        data.classList.add("unmarked-cell");
+      }
+
       data.innerHTML = bingoArray[y * 5 + x];
-      data.setAttribute("onclick", "markCell(this)");
+      data.addEventListener("click", markCell);
       row.appendChild(data);
     }
     table.appendChild(row);
@@ -58,7 +89,13 @@ function throwBoardToDOM(bingoArray) {
 
 async function initializeBingoBoard() {
   const prompts = await getBingoPrompts();
-  const bingoBoardArray = randomBingoArray(prompts);
+
+  let bingoString = localStorage.getItem("bingoString")
+    ? localStorage.getItem("bingoString")
+    : null;
+  if (!bingoString) bingoString = makeBingoString(prompts);
+
+  const bingoBoardArray = bingoArrayFromString(bingoString, prompts);
   throwBoardToDOM(bingoBoardArray);
 }
 
